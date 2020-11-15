9e6dbeb11419d94d09eb1790af6333fdfc47f007
Julian <justdotjs@icloud.com>
2020-11-15 17:54:23 +0800

    Initial Commit

---
+++ Frontend/FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendApp.swift
+//  Frontend
+//
+//  Created by Julian Schiavo on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
