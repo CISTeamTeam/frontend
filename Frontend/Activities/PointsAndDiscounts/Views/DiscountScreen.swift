f7148cc4ddcdac47ff43c5cfcde952781b2e3290
Max <mmyt96@student.cis.edu.hk>
2020-11-19 13:55:46 +0800

    Finished discount screen

--- Frontend/Activities/PointsAndDiscounts/Views/DiscountScreen.swift
+++ Frontend/Activities/PointsAndDiscounts/Views/DiscountScreen.swift
@@ -17,11 +17,19 @@ struct DiscountScreen: View, Loadable {
     // - TODO: Create the screen that shows discount details (this shows more details than DiscountView as it is shown filling the whole screen)
     // Show price, title, description, and a button to redeem the discount
     func body(with discount: Discount) -> some View {
-        VStack{
-            Text(discount.name).font(.title).padding()
-            Text(String(discount.requiredPoints) + " Points")
-                .font(.title2)
-                .fontWeight(.light)
+        VStack {
+            VStack{
+                Text(discount.name).font(.title).padding()
+                Text(String(discount.requiredPoints) + " Points")
+                    .font(.title2)
+                    .fontWeight(.light)
+                Divider()
+                Text(discount.description).padding()
+                
+            }
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Redeem")
+            }
         }
     }
 }
