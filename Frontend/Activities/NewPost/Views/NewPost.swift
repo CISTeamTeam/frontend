5ea48db37f355d28bceb305296741775e134db73
Max <mmyt96@student.cis.edu.hk>
2020-11-20 11:14:08 +0800

    added upload page ooga booga

--- Frontend/Activities/NewPost/Views/NewPost.swift
+++ Frontend/Activities/NewPost/Views/NewPost.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct NewPost: View {
+    @State private var image: UIImage?
+    
+    @State private var isImageSheetPresented = false
+    
+    @State private var description = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    
+            }
+            Button(action: { self.isImageSheetPresented.toggle() }, label: {
+                HStack{
+                    Image(systemName: "square.and.arrow.up").font(.largeTitle).frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Text("Upload Image").font(.largeTitle)
+                }
+            })
+            .sheet(isPresented: $isImageSheetPresented, content: {
+                ImagePicker(image: $image)
+            })
+            
+            Form {
+                TextField("Description: ", text: $description)
+            }
+            
+            Button(
+                action:{}, //idk what action
+                label: {
+                    HStack{
+                    Text("Post").font(.largeTitle)
+                    Image(systemName: "arrowshape.turn.up.right.fill").font(.largeTitle).frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }
+                }
+            )
+                
+        }
     }
 }
 
