3fc6b2e4509aef0dc8560f8c03f041b8eb0156e2
Julian <justdotjs@icloud.com>
2020-11-20 22:55:39 +0800

    Keys check

--- Frontend/Keys.swift
+++ Frontend/Keys.swift
@@ -1,6 +1,6 @@
 /// API Keys required for sections of the app. Not committed to Github.
 struct Keys {
-    static let imgurClientID = ""
+    static let imgurClientID = "x"
     static let googleClientID = ""
     static let googleClientSecret = ""
     static let googleRedirectURL = ""
