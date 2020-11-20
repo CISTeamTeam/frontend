8152fd91d96818bd000c15a7eb49e2fe5dee890a
Julian Schiavo <justdotJS@icloud.com>
2020-11-20 22:52:45 +0800

    Create Keys.swift

---
+++ Frontend/Keys.swift
@@ -0,0 +1,7 @@
+/// API Keys required for sections of the app. Not committed to Github.
+struct Keys {
+    static let imgurClientID = ""
+    static let googleClientID = ""
+    static let googleClientSecret = ""
+    static let googleRedirectURL = ""
+}
