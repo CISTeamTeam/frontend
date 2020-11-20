a3a5dd70f7d04693fa3ae77d6d9237338de7c47a
Julian <justdotjs@icloud.com>
2020-11-20 22:56:58 +0800

    SIWA

--- Frontend/Keys.swift
+++
@@ -1,7 +0,0 @@
-/// API Keys required for sections of the app. Not committed to Github.
-struct Keys {
-    static let imgurClientID = "x"
-    static let googleClientID = ""
-    static let googleClientSecret = ""
-    static let googleRedirectURL = ""
-}
